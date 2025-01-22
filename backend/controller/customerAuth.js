4b90e6fb04b39bdfd99709482b88f33612815126
Ankit <contactankit1234@gmail.com>
2025-01-22 21:38:30 +0530

    search bug fixed

--- backend/controller/customerAuth.js
+++ backend/controller/customerAuth.js
@@ -20,6 +20,9 @@ router.get('/bulkprovider', async (req, res) => {
         }
     });
     const providers = await prisma.provider.findMany({
+        where:{
+            available:true
+        },
         include:{
             address: true,
             feedbacks:true,
