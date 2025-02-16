8d11386301ac7ccad10b6a8fd6b9e4b83ccd6cf8
sakshamshukla02 <sshuklaknp2002@gmail.com>
2025-02-16 08:34:20 +0530

    Added history page

--- frontend/src/Pages/History/History.jsx
+++ frontend/src/Pages/History/History.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
 import NewCustomerNavbar from '../../Components/NewNavbar/NewCustomerNavbar.jsx';
 import Footer from '../../Components/Footer/Footer.jsx';
+import {useEffect} from 'react';
+import axios from 'axios';
+import { data } from 'react-router-dom';
 
 const History = () => {
+
+  useEffect(() => {
+    const fetchHistory=async()=>{
+      try{
+    let newData=JSON.stringify({userId:localStorage.getItem("currentUser")});
+    console.log(newData);
+    const url = `http://localhost:3000/customer/auth/orderhistory`;
+    console.log(url);
+    let config = {
+      method: "get",
+      maxBodyLength: Infinity,
+      url: url,
+      headers: {
+        Authorization: `bearer ${localStorage.getItem("customerToken")}`,
+      },
+        data: newData,
+      }
+      const response = await axios.request(config);
+      console.log(response.data);
+    }
+      catch(err)
+      {
+        console.log(err);
+      }
+    }
+    fetchHistory();
+  }, []);
   return (
     <>
       <NewCustomerNavbar/>
