d74993b95a57d446e5b2fc85945d0d99d961afa1
Aadi-03 <aaditvavijayvats.sre2022@gmail.com>
2025-02-26 00:25:15 +0530

    updated user profile UI

--- frontend/src/Pages/Profile/Profile.jsx
+++ frontend/src/Pages/Profile/Profile.jsx
@@ -4,6 +4,17 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import "./Profile.css";
 
+const calculateAge = (dob) => {
+    const birthDate = new Date(dob);
+    const today = new Date();
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDifference = today.getMonth() - birthDate.getMonth();
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+};
+
 const CustomerProfile = () => {
     let [user, setUser] = useState({});
 
@@ -33,15 +44,17 @@ const CustomerProfile = () => {
             <NewCustomerNavbar />
             <div className="profile-card">
                 <h2 className="profile-name">
-                    {user.firstName} {user.lastName}
+                    Hi {user.firstName}! , Welcome Back
                 </h2>
+                <div className="profile-details profile-fullname"><b>Full Name: </b>  <p>{user.firstName} {user.lastName}</p></div>
                 <div className="profile-details profile-email"><b>Email: </b>  <p>{user.email}</p></div>
                 <div className="profile-details profile-phone"><b>Phone: </b> <p>{user.phoneNumber}</p></div>
                 <div className="profile-details profile-gender"><b>Gender: </b> <p>{user.gender}</p></div>
                 <div className="profile-details profile-dob"><b>DOB: </b> <p>{new Date(user.dob).toLocaleDateString()}</p></div>
-                <div className="profile-details profile-orders"><b>Orders: </b> <p>{user?.orders?.length}</p></div>
+                <div className="profile-details profile-age"><b>Age: </b> <p>{calculateAge(user.dob)} years</p></div>
+                <div className="profile-details profile-orders"><b>Total Services Availed: </b> <p>{user?.orders?.length}</p></div>
                 <div className="profile-details profile-fav"><b>Favorites: </b> <p>{user?.favorites?.length}</p></div>
-                <div className="profile-details profile-feedbacks"><b>Feedbacks: </b> <p>{user?.feedbacks?.length}</p></div>
+                <div className="profile-details profile-feedbacks"><b>Feedbacks Given : </b> <p>{user?.feedbacks?.length}</p></div>
                 <div className="profile-address">
                     <h3 className="address-title">Address</h3>
                     <p className="address-detail">{user?.address?.houseNumber}, {user?.address?.streetName}, {user?.address?.state}, {user?.address?.country}</p>
