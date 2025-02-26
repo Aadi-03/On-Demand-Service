58c79014525965b9c12ba39f562b6108a782c0ee
Aadi-03 <aaditvavijayvats.sre2022@gmail.com>
2025-02-26 19:34:21 +0530

    added description in favorite

--- frontend/src/Pages/Favorites/Favorites.jsx
+++ frontend/src/Pages/Favorites/Favorites.jsx
@@ -13,6 +13,21 @@ import { ToastContainer, toast } from "react-toastify";
 
 const Favorites = () => {
   const [providerData, setProviderData] = useState([]);
+    const [descriptionComponent, setdescriptionComponent] = useState({
+      0 : false,
+      1 : false,
+      2 : false,
+      3 : false,
+      4 : false,
+      5 : false,
+      6 : false,
+      7 : false,
+      8 : false,
+      9 : false,
+      10 : false,
+      11 : false,
+      12 : false
+    });
   useEffect(() => {
     // console.log(localStorage.getItem("customerToken"));
 
@@ -41,6 +56,16 @@ const Favorites = () => {
 
     makeRequest();
   }, []);
+
+  const handleServiceRequest = (key) => {
+    // console.log(key)
+    setdescriptionComponent((prev) => {
+      return {
+        ...prev,
+        [key]: !prev[key]
+      }
+    });
+  };
 
   const handleRemoveBookmard = (id, name) => {
     let data = JSON.stringify({
@@ -100,16 +125,36 @@ const Favorites = () => {
                     )
                   }
                 />
-                <Card
-                  key={index}
-                  id={provider.providerId}
-                  name={provider.providerName}
-                  age={provider.providerAge}
-                //   distance={provider.providerDistanceInKm}
-                  workType={provider.providerWorkType}
-                  rating={provider.providerRating}
-                  phoneNo={provider.providerPhone}
-                />
+                <div className="provider-complete-card-holder">
+                  <div className="provider-card-container">
+                    <Card
+                      key={index}
+                      id={provider.providerId}
+                      name={provider.providerName}
+                      age={provider.providerAge}
+                      distance={provider.providerDistanceInKm}
+                      workType={provider.providerWorkType}
+                      rating={provider.providerRating}
+                      phoneNo={provider.providerPhone}
+                      onClick={() => handleCardClick(provider)}
+                    />
+                    <button className="select-worker" onClick={() => { handleServiceRequest(index) }}>Request Service</button>
+                  </div>
+
+                  { descriptionComponent[index] &&
+                    <div className="descriptionComponent">
+                      <div className="description-container">
+                        <h3>Enter Description of Task</h3>
+                        <form action="" className="description-form">
+                          <input type="text" placeholder="Enter the title" />
+                          <textarea placeholder="Enter Description" name="" id="" cols="30" rows="10"></textarea>
+                          <button type="submit">Submit</button>
+                        </form>
+                        <button className="close-button" onClick={() => { handleServiceRequest(index) }}>Close</button>
+                      </div>
+                    </div>
+                  }
+                </div>
               </div>
             ))
           ) : (
